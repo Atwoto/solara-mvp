67f794ccd14a1332619b8d0ab5b25a0fc927cf79
Atwoto <harrisonndeke304@gmail.com>
2025-06-21 15:23:07 +0300

    refactor: Centralize authOptions to fix Vercel build

--- src/test-types.ts
+++
@@ -1,7 +0,0 @@
-import type { NextAuthOptions, User, Session } from 'next-auth';
-
-let myOptions: NextAuthOptions;
-let myUser: User;
-let mySession: Session;
-
-console.log(myOptions, myUser, mySession);
